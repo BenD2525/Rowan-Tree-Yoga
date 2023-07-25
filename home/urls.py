aea362b4dff8cc3ca3ab99a8a3a4491fec51ebb5
Ben Dawson <bendawson2525@gmail.com>
2023-07-25 09:09:05 +0100

    Add templates, urls and views for home app, amend navbar to burger menu on all screens

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,7 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about_us', views.about_us, name='about_us'),
+    path('gallery', views.gallery, name='gallery'),
+    path('contact_us', views.contact_us, name='contact_us'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/index.html')
+
+def about_us(request):
+    return render(request, 'home/about_us.html')
+
+def gallery(request):
+    return render(request, 'home/gallery.html')
+
+def contact_us(request):
+    return render(request, 'home/contact_us.html')
